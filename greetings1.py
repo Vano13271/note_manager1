e23fa77ff7da05bfde32e323f0a98658caa89a23
Иван Жуков <ivan_zhukov_1999@bk.ru>
2024-12-27 00:13:14 +0800

    Add files via upload

---
+++ greetings1.py
@@ -0,0 +1,14 @@
+# присвоение значения пременным
+username= "Учеба"
+title = "не забывать"
+content = "домашка"
+status = "в работе"
+created_date ="26.12.2024"
+issue_date = "26.02.2025"
+# вывод значения переменнных
+print("Учеба",username)
+print("не забывать",title)
+print("домашка",content)
+print("в работе",status)
+print("26.12.2024",created_date)
+print("26.02.2025",issue_date)
